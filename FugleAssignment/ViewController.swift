55005a06c3ca93ae005692ea2414eb3618f83e92
Spock <spock@firstory.me>
2023-02-24 07:32:17 +0800

    start

---
+++ FugleAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FugleAssignment
+//
+//  Created by 薛宇振 on 2023/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
