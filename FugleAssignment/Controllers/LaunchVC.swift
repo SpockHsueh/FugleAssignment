fe16336e0ba7a5292c01b73a4a061002f024d8d5
Spock <spock@firstory.me>
2023-03-01 18:57:01 +0800

    clean code

--- FugleAssignment/Controllers/LaunchVC.swift
+++ FugleAssignment/Controllers/LaunchVC.swift
@@ -2,16 +2,32 @@
 //  LaunchVC.swift
 //  FugleAssignment
 //
-//  Created by 薛宇振 on 2023/2/27.
+//  Created by 薛宇振 on 2023/2/24.
 //
 
 import Foundation
 import UIKit
 
-class LaunchVC: UIViewController {
+// TODO
+class LaunchVC: UIViewController, Coordinating {
+    
+    var coordinator: Coordinator?
+    
+    private lazy var button: UIButton = {
+       let button = UIButton()
+        button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        title = "Launch"
+        self.view.backgroundColor = .red
+        self.title = "Launch"
+        
+        view.addSubview(button)
+    }
+    
+    @objc func pressed() {
+//        coordinator?.eventOccurred(with: <#T##Event#>)
     }
 }
