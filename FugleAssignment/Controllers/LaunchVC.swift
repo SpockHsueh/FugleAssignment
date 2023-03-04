4fa5170989bcfd0d2bcca6d04af9593920fac9b5
Spock <spock@firstory.me>
2023-03-04 15:37:09 +0800

    implement trackView

--- FugleAssignment/Controllers/LaunchVC.swift
+++ FugleAssignment/Controllers/LaunchVC.swift
@@ -11,23 +11,54 @@ import UIKit
 // TODO
 class LaunchVC: UIViewController, Coordinating {
     
+    // MARK: - Properties
+    
     var coordinator: Coordinator?
     
-    private lazy var button: UIButton = {
-       let button = UIButton()
-        button.addTarget(self, action: #selector(pressed), for: .touchUpInside)
-        return button
+    // MARK: - UI Component
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "placeholderImage")
+        return imageView
     }()
+    
+    lazy var loadingIdicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.medium)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        //        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+    
+    // MARK: - Lift Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
-        self.title = "Launch"
-        
-        view.addSubview(button)
+        self.view.backgroundColor = .white
+        setupConstraints()
+        loadingIdicator.startAnimating()
     }
     
-    @objc func pressed() {
-//        coordinator?.eventOccurred(with: <#T##Event#>)
+    override func viewDidDisappear(_ animated: Bool) {
+        loadingIdicator.stopAnimating()
+    }
+    
+    // MARK: - Private Function
+    
+    func setupConstraints() {
+        view.addSubview(imageView)
+        view.addSubview(loadingIdicator)
+        
+        NSLayoutConstraint.activate([
+            
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            
+            loadingIdicator.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 30),
+            loadingIdicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loadingIdicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)            
+        ])
     }
 }
